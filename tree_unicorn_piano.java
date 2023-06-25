7ae49e969738b1571839cf8082197e3f936facb5
msheenavalerie <136640103+msheenavalerie@users.noreply.github.com>
2023-06-25 15:21:08 +0700

    Refactor file structure

---
+++ tree_unicorn_piano.java
@@ -0,0 +1,185 @@
+public class PowerOfGratitude {
+	public static void main(String[] args) {
+		
+		//1
+		System.out.println("Gratitude is the power of appreciation for the blessings we have been given in our lives.");
+		
+		//2
+		System.out.println("When we show gratitude, we open our eyes to the things we often take for granted.");
+		
+		//3
+		System.out.println("By expressing our appreciation for the little things, we can unlock a new level of positive energy and focus.");
+
+		//4
+		System.out.println("Gratitude helps us to recognize and appreciate the good in our lives.");
+		
+		//5
+		System.out.println("It helps us stay present in the moment and enjoy the here and now.");
+		
+		//6
+		System.out.println("Expressing gratitude helps us to stay humble, deepen relationships, and increase our sense of fulfillment in life.");
+		
+		//7
+		System.out.println("It can be as simple as a kind word or a hug, or as powerful as a heartfelt thank you.");
+		
+		//8
+		System.out.println("By making gratitude a daily practice, we can cultivate an attitude of appreciation that will have a profound impact on our lives.");
+		
+		//9
+		System.out.println("It can be easy to get caught up in our worries and anxieties, but when we take time to express our gratitude it helps to shift our perspective into one of positivity and optimism.");
+		
+		//10
+		System.out.println("Writing in a gratitude journal is a great way to start cultivating an attitude of gratitude.");
+		
+		//11
+		System.out.println("Taking time each day to think of one thing that you are grateful for can help to boost your mood and well-being.");
+		
+		//12
+		System.out.println("Making a conscious effort to express your gratitude to those around you can help to strengthen relationships and create more meaningful connections.");
+		
+		//13
+		System.out.println("Gratitude can help us to be more present, be thankful for what we have, and become more mindful in the everyday moments of our lives.");
+		
+		//14
+		System.out.println("It can help us to appreciate the small things and find joy in the big things.");
+		
+		//15
+		System.out.println("By practicing gratitude on a daily basis, we can tap into the power of appreciation and create a more positive mindset.");
+		
+		//16
+		System.out.println("Expressing gratitude can help us to be more aware of the beauty of the world around us and the people in our lives.");
+		
+		//17
+		System.out.println("It can help us to cultivate an attitude of contentment and satisfaction in our lives and open our hearts to the wonders of the world.");
+		
+		//18
+		System.out.println("Gratitude can also help to reduce stress and worry, as it can help to keep our focus on the present moment and appreciate the good in our lives.");
+		
+		//19
+		System.out.println("When we show gratitude, it helps us to remember the positive things that we often take for granted and to be more mindful of how we use our time and resources.");
+		
+		//20
+		System.out.println("The power of gratitude is a tool to draw on when we need it most, and by making it a part of our lives, we can reap its many benefits and live more fulfilling lives.");
+		
+		//21
+		System.out.println("The practice of gratitude can be transformative, both in our lives and in the lives of those around us.");
+		
+		//22
+		System.out.println("Gratitude can not only lead to greater happiness and contentment, but it can also help us to become better people and make our world a better place.");
+		
+		//23
+		System.out.println("Take time each day to reflect on what you are thankful for and practice expressing your appreciation to those around you.");
+		
+		//24
+		System.out.println("By expressing our gratitude, not only can we unlock the power of appreciation, but we can also bring a little more joy into our lives and the lives of those around us.");
+		
+		//25
+		System.out.println("There is no limit to the power of gratitude, and it is there for us whenever we choose to use it.");
+		
+		//26
+		System.out.println("When we take the time to pause and express our appreciation for the things in our lives, we open ourselves up to more positive energy and wellbeing.");
+		
+		//27
+		System.out.println("Gratitude can help us to look past our current circumstances and recognize the beauty and abundance in our lives.");
+		
+		//28
+		System.out.println("It can help us to be mindful of our blessings and appreciate each moment as it comes.");
+		
+		//29
+		System.out.println("Practicing gratitude can be a powerful way to shift our mindset and create a life filled with more joy and fulfillment.");
+		
+		//30
+		System.out.println("The power of gratitude can bring us moments of peace and help us to recognize and appreciate the beauty of the world around us.");
+		
+		//31
+		System.out.println("Take time to express your appreciation for the people and experiences in your life and appreciate all that has been given to you.");
+		
+		//32
+		System.out.println("By being grateful for the good we have, we can open ourselves up to more positive energy and create a life of abundance and joy.");
+		
+		//33
+		System.out.println("The power of gratitude can help us to stay focused on the present moment and recognize the beauty of the here and now.");
+		
+		//34
+		System.out.println("By expressing our thanks and appreciation, we can unlock a new level of joy and contentment in our lives.");
+		
+		//35
+		System.out.println("Take time each day to pause and appreciate the little things, and find ways to express your gratitude to those around you.");
+		
+		//36
+		System.out.println("Gratitude can be a powerful tool to unlock more joy and abundance in our lives and to appreciate the beauty of the world around us.");
+		
+		//37
+		System.out.println("Make gratitude a part of your daily practice and reap the rewards of a life filled with more joy and appreciation.");
+		
+		//38
+		System.out.println("The power of gratitude can open us up to new possibilities and lead us to a more meaningful and fulfilled life.");
+		
+		//39
+		System.out.println("It can be as simple as writing a thank you note or sending a kind word, or as powerful as expressing your appreciation for the blessings in your life.");
+		
+		//40
+		System.out.println("By cultivating an attitude of gratitude we can open ourselves up to more joy and optimism and create a life filled with more abundance and contentment.");
+		
+		//41
+		System.out.println("Gratitude can help us to stay present in the moment and enjoy the beauty of the here and now.");
+		
+		//42
+		System.out.println("It can help us to recognize and appreciate all that has been given to us and to stay humble and open in our daily lives.");
+		
+		//43
+		System.out.println("Take time each day to express your thanks and appreciation to those around you and to yourself.");
+		
+		//44
+		System.out.println("By being mindful of our blessings, we can unlock a new level of joy in our lives and create more meaningful connections with those around us.");
+		
+		//45
+		System.out.println("The power of gratitude is something we can draw on whenever we need it, and by practicing gratitude on a daily basis, we can cultivate an attitude of appreciation and lead more fulfilling lives.");
+		
+		//46
+		System.out.println("Take time each day to pause and express your gratitude for all that has been given to you, and to send out your thanks to the people who make your life meaningful.");
+		
+		//47
+		System.out.println("The power of gratitude is within us, and it can be a powerful tool for creating a life of joy and abundance.");
+		
+		//48
+		System.out.println("By expressing our thanks and appreciation, we can open ourselves up to more positive energy and opportunities.");
+		
+		//49
+		System.out.println("The practice of gratitude can help us to stay in the present moment, be more mindful of our blessings, and enjoy the beauty of the here and now.");
+		
+		//50
+		System.out.println("The power of gratitude is something we can tap into anytime, and it is there for us whenever we choose to use it.");
+		
+		//51
+		System.out.println("Gratitude can help us to become more aware of the good in our lives and to appreciate the little things.");
+		
+		//52
+		System.out.println("It can help us to recognize our blessings and stay humble in our daily lives.");
+		
+		//53
+		System.out.println("Expressing our thanks to those around us can help to deepen relationships and create more meaningful connections.");
+		
+		//54
+		System.out.println("Take time to express your gratitude and reap the rewards of a life filled with more positivity and joy.");
+		
+		//55
+		System.out.println("The power of gratitude can help us to focus on the here and now and to appreciate the beauty of the world around us.");
+		
+		//56
+		System.out.println("By making gratitude a part of our daily lives, we can create a life filled with more appreciation and contentment.");
+		
+		//57
+		System.out.println("It can be as simple as a kind word or a hug, or as powerful as sharing your appreciation for the good in your life.");
+		
+		//58
+		System.out.println("Gratitude can help us to stay grounded, be more mindful, and find joy in the everyday moments of our lives.");
+		
+		//59
+		System.out.println("The power of gratitude can be transformative, both in our own lives and in the lives of those around us.");
+		
+		//60
+		System.out.println("Take time each day to express your thanks for what you have been given and to appreciate all that has been given to you.");
+	}
+
+}
