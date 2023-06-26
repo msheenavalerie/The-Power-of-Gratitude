c7f180b31e62447935b2ae326ea5eb396d291b2c
msheenavalerie <136640103+msheenavalerie@users.noreply.github.com>
2023-06-27 05:28:51 +0700

    Improve code efficiency

---
+++ yellow_tree_mountain.scala
@@ -0,0 +1,179 @@
+//max number of lines is 2000
+
+/*
+The Power of Gratitude
+
+Scala Code
+
+//1
+object Gratitude {
+  def main(args: Array[String]): Unit = {
+    println("Feeling grateful is one of the most powerful feelings one can experience.")
+  }
+}
+
+//2
+import scala.util.Random
+
+object Gratitude {
+  def main(args: Array[String]): Unit = {
+    println("Feeling grateful is one of the most powerful feelings one can experience.")
+    println(Random.shuffle(Seq("Thank you!", "I appreciate you!", "I'm so lucky to have you in my life.",
+    "Your actions mean a lot to me.")))
+  }
+}
+
+//3
+import scala.util.Random
+
+object Gratitude {
+  def main(args: Array[String]): Unit = {
+    println("Feeling grateful is one of the most powerful feelings one can experience.")
+    println(Random.shuffle(Seq("Thank you!", "I appreciate you!", "I'm so lucky to have you in my life.",
+    "Your actions mean a lot to me.")))
+
+    val actsOfGratitude = Seq("Write a gratitude journal", "Write a thank-you letter",
+                              "Give a hug", "Buy a gift for someone you appreciate","Smile and look people in the eye")
+    println(Random.shuffle(actsOfGratitude))
+  }
+}
+
+//4
+import scala.util.Random
+
+object Gratitude {
+  def main(args: Array[String]): Unit = {
+    println("Feeling grateful is one of the most powerful feelings one can experience.")
+    println(Random.shuffle(Seq("Thank you!", "I appreciate you!", "I'm so lucky to have you in my life.",
+    "Your actions mean a lot to me.")))
+
+    val actsOfGratitude = Seq("Write a gratitude journal", "Write a thank-you letter",
+                              "Give a hug", "Buy a gift for someone you appreciate","Smile and look people in the eye")
+    println(Random.shuffle(actsOfGratitude))
+
+    println("One way to cultivate gratitude is to practice mindful awareness. This means being in the present moment and fully appreciating it.")
+  }
+}
+
+//5
+import scala.util.Random
+
+object Gratitude {
+  def main(args: Array[String]): Unit = {
+    println("Feeling grateful is one of the most powerful feelings one can experience.")
+    println(Random.shuffle(Seq("Thank you!", "I appreciate you!", "I'm so lucky to have you in my life.",
+    "Your actions mean a lot to me.")))
+
+    val actsOfGratitude = Seq("Write a gratitude journal", "Write a thank-you letter",
+                              "Give a hug", "Buy a gift for someone you appreciate","Smile and look people in the eye")
+    println(Random.shuffle(actsOfGratitude))
+
+    println("One way to cultivate gratitude is to practice mindful awareness. This means being in the present moment and fully appreciating it.")
+    println("Take a few moments to be mindful. Focus on your breathing, observe your environment, and take note of things you are grateful for.")
+  }
+}
+
+//6
+import scala.util.Random
+
+object Gratitude {
+  def main(args: Array[String]): Unit = {
+    println("Feeling grateful is one of the most powerful feelings one can experience.")
+    println(Random.shuffle(Seq("Thank you!", "I appreciate you!", "I'm so lucky to have you in my life.",
+    "Your actions mean a lot to me.")))
+
+    val actsOfGratitude = Seq("Write a gratitude journal", "Write a thank-you letter",
+                              "Give a hug", "Buy a gift for someone you appreciate","Smile and look people in the eye")
+    println(Random.shuffle(actsOfGratitude))
+
+    println("One way to cultivate gratitude is to practice mindful awareness. This means being in the present moment and fully appreciating it.")
+    println("Take a few moments to be mindful. Focus on your breathing, observe your environment, and take note of things you are grateful for.")
+    println("At the end of the day, take a few minutes to write down three things you are grateful for or three acts of kindness you experienced.")
+  }
+}
+
+//7
+import scala.util.Random
+
+object Gratitude {
+  def main(args: Array[String]): Unit = {
+    println("Feeling grateful is one of the most powerful feelings one can experience.")
+    println(Random.shuffle(Seq("Thank you!", "I appreciate you!", "I'm so lucky to have you in my life.",
+    "Your actions mean a lot to me.")))
+
+    val actsOfGratitude = Seq("Write a gratitude journal", "Write a thank-you letter",
+                              "Give a hug", "Buy a gift for someone you appreciate","Smile and look people in the eye")
+    println(Random.shuffle(actsOfGratitude))
+
+    println("One way to cultivate gratitude is to practice mindful awareness. This means being in the present moment and fully appreciating it.")
+    println("Take a few moments to be mindful. Focus on your breathing, observe your environment, and take note of things you are grateful for.")
+    println("At the end of the day, take a few minutes to write down three things you are grateful for or three acts of kindness you experienced.")
+    println("Take a moment to reflect on your actions and how they affected others. How did your actions help someone today?")
+  }
+}
+
+//8
+import scala.util.Random
+
+object Gratitude {
+  def main(args: Array[String]): Unit = {
+    println("Feeling grateful is one of the most powerful feelings one can experience.")
+    println(Random.shuffle(Seq("Thank you!", "I appreciate you!", "I'm so lucky to have you in my life.",
+    "Your actions mean a lot to me.")))
+
+    val actsOfGratitude = Seq("Write a gratitude journal", "Write a thank-you letter",
+                              "Give a hug", "Buy a gift for someone you appreciate","Smile and look people in the eye")
+    println(Random.shuffle(actsOfGratitude))
+
+    println("One way to cultivate gratitude is to practice mindful awareness. This means being in the present moment and fully appreciating it.")
+    println("Take a few moments to be mindful. Focus on your breathing, observe your environment, and take note of things you are grateful for.")
+    println("At the end of the day, take a few minutes to write down three things you are grateful for or three acts of kindness you experienced.")
+    println("Take a moment to reflect on your actions and how they affected others. How did your actions help someone today?")
+    println("Try to practice gratitude every day. Over time, you will start to notice a change in yourself and the people around you.")
+  }
+}
+
+//9
+import scala.util.Random
+
+object Gratitude {
+  def main(args: Array[String]): Unit = {
+    println("Feeling grateful is one of the most powerful feelings one can experience.")
+    println(Random.shuffle(Seq("Thank you!", "I appreciate you!", "I'm so lucky to have you in my life.",
+    "Your actions mean a lot to me.")))
+
+    val actsOfGratitude = Seq("Write a gratitude journal", "Write a thank-you letter",
+                              "Give a hug", "Buy a gift for someone you appreciate","Smile and look people in the eye")
+    println(Random.shuffle(actsOfGratitude))
+
+    println("One way to cultivate gratitude is to practice mindful awareness. This means being in the present moment and fully appreciating it.")
+    println("Take a few moments to be mindful. Focus on your breathing, observe your environment, and take note of things you are grateful for.")
+    println("At the end of the day, take a few minutes to write down three things you are grateful for or three acts of kindness you experienced.")
+    println("Take a moment to reflect on your actions and how they affected others. How did your actions help someone today?")
+    println("Try to practice gratitude every day. Over time, you will start to notice a change in yourself and the people around you.")
+    println("Expressing gratitude is one of the most powerful tools we have to cultivate happiness in our lives. Take the time to appreciate what you have and be thankful for those who have done something nice for you.")
+  }
+}
+
+//10
+import scala.util.Random
+
+object Gratitude {
+  def main(args: Array[String]): Unit = {
+    println("Feeling grateful is one of the most powerful feelings one can experience.")
+    println(Random.shuffle(Seq("Thank you!", "I appreciate you!", "I'm so lucky to have you in my life.",
+    "Your actions mean a lot to me.")))
+
+    val actsOfGratitude = Seq("Write a gratitude journal", "Write a thank-you letter",
+                              "Give a hug", "Buy a gift for someone you appreciate","Smile and look people in the eye")
+    println(Random.shuffle(actsOfGratitude))
+
+    println("One way to cultivate gratitude is to practice mindful awareness. This means being in the present moment and fully appreciating it.")
+    println("Take a few moments to be mindful. Focus on your breathing, observe your environment, and take note of things you are grateful for.")
+    println("At the end of the day, take a few minutes to write down three things you are grateful for or three acts of kindness you experienced.")
+    println("Take a moment to reflect on your actions and how they affected others. How did your actions help someone today?")
+    println("Try to practice gratitude every day. Over time, you will start to notice a change in yourself and the people around you.")
+    println("Expressing gratitude is one of the most powerful tools we have to cultivate happiness in our lives. Take the time to appreciate what you have and be thankful for those who have done something nice for you.")
+    println("Gratitude is a way of life. By being grateful for the small things, we can start to look on the bright side of life and appreciate all that we have. So take a moment to be thankful today!")
+  }
+}
