ec853b941bb692f05fdabf270232e7a659749e4b
msheenavalerie <136640103+msheenavalerie@users.noreply.github.com>
2023-06-26 23:25:46 +0700

    Optimize code compilation

---
+++ dog_ocean_waterfall.js
@@ -0,0 +1,43 @@
+// Create an array containing 2000 different messages about the power of gratitude
+const messages = [
+    'Gratitude is the best attitude!',
+    'Gratitude makes us aware of what we have, and not what we lack.',
+    'Gratitude is the single most powerful force for making life greater than it already is.',
+    'Gratitude for the present moment and the fullness of life now is true prosperity.',
+    'Gratitude helps us become more aware of the abundance that exists in our lives.',
+    'Gratitude creates a positive attitude that leads to a happier, more prosperous life.',
+    'Gratitude helps us stay in tune with the world around us and keep our worries in perspective.',
+    'Gratitude helps us appreciate what is, not what was or what could be.',
+    'Gratitude is the practice of being thankful and expressing appreciation for everything in your life.',
+    'Gratitude is a choice, and it affects how we think, feel, and act in our daily lives.',
+    'Gratitude opens our hearts to the good in life, allowing us to accept and receive more.',
+    'Gratitude sharpens our awareness and helps us be more mindful in our every day.',
+    'Gratitude helps us become more conscious of the good things in our lives.',
+    'It creates positive relationships with the world around us and increases our self-esteem.',
+    'Gratitude brings a greater level of satisfaction, joy and peace of mind to our lives.',
+    'Gratitude helps us to focus on the good, rather than dwelling on the bad or the not-so-good.',
+    'Gratitude encourages us to take time to appreciate our successes and our blessings.',
+    'Gratitude allows us to experience joy, no matter what our circumstances may be.',
+    'Gratitude shifts our focus to the present moment, helping us to see all the good in our lives.',
+    'When we give thanks, we open ourselves up to contentment and peace that can’t be found elsewhere.',
+    'Gratitude helps us to tap into our inner strength and brings more meaning and purpose to our lives.',
+    'When we look at life through the eyes of gratitude, we can find beauty and joy in the little things.',
+    'Gratitude connects us to our inner power, helping us to live more abundantly and authentically.',
+    'Gratitude is a catalyst for becoming more compassionate, loving and generous in all we do.',
+    'Gratitude is the antidote to envy, bitterness, and despair, bringing more peace and hope to our lives.',
+    'Gratitude is the key that unlocks the door to a richer, more fulfilling life, full of purpose and joy.',
+    'Gratitude makes us more alive to the world around us, and more in tune with its beauty and wonder.',
+    'Gratitude is the foundation of optimism, helping us to find the silver lining in each and every situation.',
+    'Gratitude helps us to learn from our mistakes and move forward with greater wisdom and courage.',
+    'Gratitude encourages us to look for the good and trust that life is working out for our highest good.'
+];
+
+// Create a function that will loop through the array and console log each message
+function showPowerOfGratitude() {
+    for (let i = 0; i < messages.length; i++) {
+        console.log(messages[i]);
+    }
+}
+
+// Invoke the function
+showPowerOfGratitude();
