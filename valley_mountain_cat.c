c1a77bf7381adfda7b1a3d2908af7d5794a4c88b
msheenavalerie <136640103+msheenavalerie@users.noreply.github.com>
2023-06-27 12:23:51 +0700

    Add error messages

---
+++ valley_mountain_cat.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("The Power of Gratitude\n\n");
+    
+    printf("Gratitude is the single most powerful way to improve your life. It enables us to celebrate and appreciate the blessings we have and the successes we achieve. It gives us hope and inspires us to reach for more. When we express gratitude, the universe responds with more of the same.\n\n");
+
+    printf("When we are feeling thankful and appreciative our vibration is lifted. We tap into abundance and attract more of what we want into our lives. We become more mindful and recognize the good in our lives – even in the most challenging circumstances.\n\n");
+
+    printf("Gratitude has an illusive power. It can shift your perspective in an instant. It can motivate you to act and energize you to take the steps necessary to get what you want. It can calm your mind and soothe your soul.\n\n");
+
+    printf("Just like any other emotion, gratitude requires practice. Start by simply writing down three things each day that you are thankful for. Then expand your list – what you are thankful for in your life, your relationships, your job, your health…the list is endless.\n\n");
+
+    printf("Take it a step further and express your gratitude to those in your life. Let them know how much you appreciate them and all that they do. A simple thank you can make a world of difference.\n\n");
+
+    printf("Gratitude for what we have and what we could become has the power to transform our lives in a positive way. Incorporating gratitude into our daily life opens us up to countless possibilities.\n\n");
+
+    printf("The power of gratitude will change your life forever.\n\n");
+
+    return 0;
+
+}
