6dc5c760003525dd8245eeef81452d91e63f1bca
msheenavalerie <136640103+msheenavalerie@users.noreply.github.com>
2023-07-17 08:19:44 +0700

    Improve data validation

---
+++ tree_ocean_dog.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+
+public class Gratitude {
+	
+	public static void main(String[] args) {
+
+        // Create an ArrayList of String objects to store gratitude statements
+        ArrayList<String> gratitudeStatements = new ArrayList<String>();
+
+        // Add gratitude statements to ArrayList
+        gratitudeStatements.add("I am grateful for this day.");
+        gratitudeStatements.add("I am grateful for the people in my life.");
+        gratitudeStatements.add("I am grateful for my health and well-being.");
+        gratitudeStatements.add("I am grateful for the chance to learn and grow.");
+        gratitudeStatements.add("I am grateful for the beauty of nature.");
+        gratitudeStatements.add("I am grateful for the food I eat and the roof over my head.");
+        gratitudeStatements.add("I am grateful for the moments of joy and love in my life.");
+        gratitudeStatements.add("I am grateful for my ability to make a difference.");
+        gratitudeStatements.add("I am grateful for the kindness of others.");
+        gratitudeStatements.add("I am grateful for the wisdom I have gained.");
+        gratitudeStatements.add("I am grateful for the opportunities I have been given.");
+        gratitudeStatements.add("I am grateful for the freedom I enjoy.");
+        gratitudeStatements.add("I am grateful to be alive and experience the world.");
+        gratitudeStatements.add("I am grateful to have a positive attitude.");
+        gratitudeStatements.add("I am grateful for the strength I have within.");
+        gratitudeStatements.add("I am grateful for the laughter I share with others.");
+        gratitudeStatements.add("I am grateful for the miracles that come my way.");
+        gratitudeStatements.add("I am grateful for the calmness in my heart.");
+        gratitudeStatements.add("I am grateful for the new roads that open up before me.");
+        gratitudeStatements.add("I am grateful for the challenges that I overcome.");
+        gratitudeStatements.add("I am grateful for the dreams I have and the courage to pursue them.");
+        gratitudeStatements.add("I am grateful for the chance to become the best version of myself.");
+        gratitudeStatements.add("I am grateful for the help I receive along the way.");
+        gratitudeStatements.add("I am grateful for the faith that sustains me.");
+        gratitudeStatements.add("I am grateful for the compassion I show and receive.");
+        gratitudeStatements.add("I am grateful for the love that surrounds me.");
+        gratitudeStatements.add("I am grateful for the courage to take risks.");
+        gratitudeStatements.add("I am grateful for the creativity that flows through me.");
+        gratitudeStatements.add("I am grateful for the support I can give to others.");
+        gratitudeStatements.add("I am grateful for the peace that comes from within.");
+        gratitudeStatements.add("I am grateful for the moments of stillness that I experience.");
+        gratitudeStatements.add("I am grateful for the balance I find in life.");
+        gratitudeStatements.add("I am grateful for the abundance that fills my life.");
+        gratitudeStatements.add("I am grateful for the beauty of life around me.");
+        gratitudeStatements.add("I am grateful for the kindness that comes back to me.");
+        gratitudeStatements.add("I am grateful for the growth that comes from change.");
+        gratitudeStatements.add("I am grateful for the joy that comes from within.");
+        gratitudeStatements.add("I am grateful for the love I have for myself and others.");
+        gratitudeStatements.add("I am grateful for the clarity I find during moments of stillness.");
+        gratitudeStatements.add("I am grateful for the moments of appreciation that I share with others.");
+        gratitudeStatements.add("I am grateful for the understanding that comes with acceptance.");
+        gratitudeStatements.add("I am grateful for the joy of helping others.");
+        gratitudeStatements.add("I am grateful for the hope that springs from within.");
+        gratitudeStatements.add("I am grateful for the freedom I feel when I choose love.");
+        gratitudeStatements.add("I am grateful for the trust that I have in myself.");
+        gratitudeStatements.add("I am grateful for the knowledge I have gained and will continue to gain.");
+        gratitudeStatements.add("I am grateful for the peace of mind I enjoy.");
+        gratitudeStatements.add("I am grateful for the courage that comes with vulnerability.");
+        gratitudeStatements.add("I am grateful for the power of gratitude.");
+
+        // Print out all of the gratitude statements
+        for (String statement : gratitudeStatements) {
+            System.out.println(statement);
+        }
+	}
+
+}
