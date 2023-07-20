de113500cea2f1928a8569d2a70c46d38e6982a1
msheenavalerie <136640103+msheenavalerie@users.noreply.github.com>
2023-07-20 16:26:24 +0700

    Refactor variable names

---
+++ ocean_valley_king.scala
@@ -0,0 +1,503 @@
+object ThePowerOfGratitude {
+
+  // 1
+  def expressGratitude(): Unit = {
+    println("I am grateful for...")
+  }
+
+  // 2
+  def promptThoughts(): Unit = {
+    println("What are you grateful for today?")
+  }
+
+  // 3
+  def takeNotice(): Unit = {
+    println("Take a few moments to really focus on appreciation")
+  }
+
+  // 4
+  def appreciate(): Unit = {
+    println("Gratitude is best expressed with intention and appreciation.")
+  }
+
+  // 5
+  def recognizeBlessings(): Unit = {
+    println("Recognize the blessings that are part of your life.")
+  }
+
+  // 6
+  def cantTakeForGranted(): Unit = {
+    println("Look around and recognize all that you can't take for granted.")
+  }
+
+  // 7
+  def benefitGivers(): Unit = {
+    println("Be thankful for those who have contributed to your well-being.")
+  }
+
+  // 8
+  def commitGratitude(): Unit = {
+    println("Commit to being thankful no matter what happens.")
+  }
+
+  // 9
+  def everydayGrateful(): Unit = {
+    println("Be grateful every day.")
+  }
+
+  // 10
+  def meaningfulThankYou(): Unit = {
+    println("Express your gratitude with a meaningful 'thank you'.")
+  }
+
+  // 11
+  def reachOut(): Unit = {
+    println("Reach out to others to share your appreciation.")
+  }
+
+  // 12
+  def transparentThankful(): Unit = {
+    println("Be transparent about the things you're thankful for.")
+  }
+
+  // 13
+  def gratitudeJournal(): Unit = {
+    println("Start a gratitude journal to keep track of your blessings.")
+  }
+
+  // 14
+  def spreadGratitude(): Unit = {
+    println("Spread feelings of gratitude around you.")
+  }
+
+  // 15
+  def livingGratitude(): Unit = {
+    println("Make living in gratitude a habit.")
+  }
+
+  // 16
+  def recognizingBenefits(): Unit = {
+    println("Start recognizing the benefits of being grateful.")
+  }
+
+  // 17
+  def findingJoy(): Unit = {
+    println("Look for the joy in the small things.")
+  }
+
+  // 18
+  def appreciatingAround(): Unit = {
+    println("Appreciate the people and things around you.")
+  }
+
+  // 19
+  def enjoyingLife(): Unit = {
+    println("Allow yourself to enjoy life.")
+  }
+
+  // 20
+  def realizeExternalCircumstances(): Unit = {
+    println("Realize that external circumstances do not determine your worth.")
+  }
+
+  // 21
+  def expressThanks(): Unit = {
+    println("Express your gratitude when given compliments.")
+  }
+
+  // 22
+  def listenGratitude(): Unit = {
+    println("Listen to what people say and internalize it.")
+  }
+
+  // 23
+  def cultivateAttitude(): Unit = {
+    println("Cultivate an attitude of gratitude.")
+  }
+
+  // 24
+  def sayThankYou(): Unit = {
+    println("Say 'thank you' as often as possible.")
+  }
+
+  // 25
+  def enjoyMoment(): Unit = {
+    println("Take a moment to enjoy the blessings you have.")
+  }
+
+  // 26
+  def practiceGratitude(): Unit = {
+    println("Practice being grateful every day.")
+  }
+
+  // 27
+  def noticeLittleThings(): Unit = {
+    println("Notice the little things and see the beauty in them.")
+  }
+
+  // 28
+  def beGrateful(): Unit = {
+    println("Be grateful for what you have, no matter how small.")
+  }
+
+  // 29
+  def treatPeopleKindly(): Unit = {
+    println("Treat people kindly and show appreciation for their efforts.")
+  }
+
+  // 30
+  def recognizeSmallVictories(): Unit = {
+    println("Recognize and celebrate even the smallest victories.")
+  }
+
+  // 31
+  def takeAction(): Unit = {
+    println("Take action to show your appreciation.")
+  }
+
+  // 32
+  def thankPeople(): Unit = {
+    println("Thank people who have helped you.")
+  }
+
+  // 33
+  def experiencePower(): Unit = {
+    println("Experience the power of gratitude first hand.")
+  }
+
+  // 34
+  def shareBlessings(): Unit = {
+    println("Share your blessings with those in need.")
+  }
+
+  // 35
+  def positiveEmotions(): Unit = {
+    println("Express your positive emotions to others.")
+  }
+
+  // 36
+  def lookForwards(): Unit = {
+    println("Look forward to the future and all that it can bring.")
+  }
+
+  // 37
+  def thankYouLetter(): Unit = {
+    println("Take the time to write a thank you letter to someone special.")
+  }
+
+  // 38
+  def bePresent(): Unit = {
+    println("Be present in the moment and appreciate what you have.")
+  }
+
+  // 39
+  def keepingPerspective(): Unit = {
+    println("Keep perspective on the big picture of life.")
+  }
+
+  // 40
+  def frameThoughts(): Unit = {
+    println("Frame your thoughts and focus on the positive.")
+  }
+
+  // 41
+  def gratefulActions(): Unit = {
+    println("Take action to express your feelings of gratitude.")
+  }
+
+  // 42
+  def shareExperience(): Unit = {
+    println("Share your gratitude experiences with others.")
+  }
+
+  // 43
+  def recognizeAchievements(): Unit = {
+    println("Recognize your personal achievements and those of others.")
+  }
+
+  // 44
+  def beInspired(): Unit = {
+    println("Be inspired by those around you.")
+  }
+
+  // 45
+  def embraceLife(): Unit = {
+    println("Embrace life with an attitude of gratitude.")
+  }
+
+  // 46
+  def createMemories(): Unit = {
+    println("Create special memories with your loved ones.")
+  }
+
+  // 47
+  def giveBack(): Unit = {
+    println("Give back to your community with your time and energy.")
+  }
+
+  // 48
+  def practiceKindness(): Unit = {
+    println("Practice kindness and offer appreciation to others.")
+  }
+
+  // 49
+  def countBlessings(): Unit = {
+    println("Count your blessings each day.")
+  }
+
+  // 50
+  def findContentment(): Unit = {
+    println("Find contentment in life and be thankful for it.")
+  }
+
+  // 51
+  def expressGratitudeDaily(): Unit = {
+    println("Express your gratitude daily.")
+  }
+
+  // 52
+  def smileOften(): Unit = {
+    println("Smile often and be grateful for the good things in life.")
+  }
+
+  // 53
+  def extendGrateful(): Unit = {
+    println("Extend your gratefulness to those around you.")
+  }
+
+  // 54
+  def pauseToReflect(): Unit = {
+    println("Pause to reflect on all that you have been given.")
+  }
+
+  // 55
+  def extendGenerosity(): Unit = {
+    println("Extend your generosity to those in need.")
+  }
+
+  // 56
+  def spreadJoy(): Unit = {
+    println("Spread joy and appreciation to everyone you meet.")
+  }
+
+  // 57
+  def shareGifts(): Unit = {
+    println("Share the gifts you have been given with others.")
+  }
+
+  // 58
+  def inspireOthers(): Unit = {
+    println("Inspire others to be grateful too.")
+  }
+
+  // 59
+  def giveEveryone(): Unit = {
+    println("Give everyone the gift of gratitude.")
+  }
+
+  // 60
+  def demonstrateActs(): Unit = {
+    println("Demonstrate acts of gratitude in your daily life.")
+  }
+
+  // 61
+  def understandBlessings(): Unit = {
+    println("Understand the blessings you have been given.")
+  }
+
+  // 62
+  def appreciateGenerosity(): Unit = {
+    println("Appreciate all the generosity you encounter.")
+  }
+
+  // 63
+  def createRitual(): Unit = {
+    println("Create a gratitude ritual for yourself.")
+  }
+
+  // 64
+  def engageCommunity(): Unit = {
+    println("Engage in your community and contribute to its well-being.")
+  }
+
+  // 65
+  def takeActionGratitude(): Unit = {
+    println("Take action when you feel grateful.")
+  }
+
+  // 66
+  def giveSomeone(): Unit = {
+    println("Give someone a genuine compliment and thank them for their efforts.")
+  }
+
+  // 67
+  def recognizeSpecial(): Unit = {
+    println("Recognize something special in yourself and those around you.")
+  }
+
+  // 68
+  def reflectOnGood(): Unit = {
+    println("Reflect on the good things in your life.")
+  }
+
+  // 69
+  def appreciateNature(): Unit = {
+    println("Appreciate the beauty of nature around you.")
+  }
+
+  // 70
+  def everydayThankful(): Unit = {
+    println("Be thankful for all that you have, every day.")
+  }
+
+  // 71
+  def recognizeAchievement(): Unit = {
+    println("Recognize your achievements and celebrate them.")
+  }
+
+  // 72
+  def acknowledgeGrowth(): Unit = {
+    println("Acknowledge your growth and progress.")
+  }
+
+  // 73
+  def beHumble(): Unit = {
+    println("Be humble in your success and show gratitude.")
+  }
+
+  // 74
+  def beThoughtful(): Unit = {
+    println("Be thoughtful in how you express yourself.")
+  }
+
+  // 75
+  def appreciateProcess(): Unit = {
+    println("Appreciate the process and all the challenges it brought.")
+  }
+
+  // 76
+  def recognizeOpportunities(): Unit = {
+    println("Recognize the opportunities that come your way.")
+  }
+
+  // 77
+  def identifyLessons(): Unit = {
+    println("Identify the lessons learned from difficult times.")
+  }
+
+  // 78
+  def takeResponsibility(): Unit = {
+    println("Take responsibility for your mistakes and learn from them.")
+  }
+
+  // 79
+  def acknowledgeHelpers(): Unit = {
+    println("Acknowledge and thank the people who helped you.")
+  }
+
+  // 80
+  def understandJourney(): Unit = {
+    println("Understand the journey and how far you have come.")
+  }
+
+  // 81
+  def expressAppreciation(): Unit = {
+    println("Express your appreciation for life and all it has to offer.")
+  }
+
+  // 82
+  def appreciateSuccess(): Unit = {
+    println("Appreciate your successes and use them as motivation.")
+  }
+
+  // 83
+  def practiceReward(): Unit = {
+    println("Practice rewarding yourself for your hard work.")
+  }
+
+  // 84
+  def keepSenseHumor(): Unit = {
+    println("Keep a sense of humor and laugh at yourself.")
+  }
+
+  // 85
+  def motivateOthers(): Unit = {
+    println("Motivate others with your positive attitude.")
+  }
+
+  // 86
+  def beCompassionate(): Unit = {
+    println("Be compassionate with yourself and others.")
+  }
+
+  // 87
+  def embodyGratitude(): Unit = {
+    println("Embody gratitude in your everyday life.")
+  }
+
+  // 88
+  def givePositiveEnergy(): Unit = {
+    println("Give positive energy to all those around you.")
+  }
+
+  // 89
+  def thankThoughts(): Unit = {
+    println("Thank your thoughts for guiding you through life.")
+  }
+
+  // 90
+  def gladdenHeart(): Unit = {
+    println("Allow your heart to be gladdened by moments of joy.")
+  }
+
+  // 91
+  def fillHeart(): Unit = {
+    println("Fill your heart with love, joy, and gratitude.")
+  }
+
+  // 92
+  def celebrateLife(): Unit = {
+    println("Celebrate life and all its wonders.")
+  }
+
+  // 93
+  def spreadLove(): Unit = {
+    println("Spread love and show appreciation for all beings.")
+  }
+
+  // 94
+  def openMind(): Unit = {
+    println("Open your mind to new possibilities.")
+  }
+
+  // 95
+  def believeGood(): Unit = {
+    println("Believe in the good and all that is possible.")
+  }
+
+  // 96
+  def nurtureInnerSelf(): Unit = {
+    println("Nurture your inner self and find peace within.")
+  }
+
+  // 97
+  def recognizeGifts(): Unit = {
+    println("Recognize the gifts in everyone and everything.")
+  }
+
+  // 98
+  def appreciateUnique(): Unit = {
+    println("Appreciate the unique qualities in everyone.")
+  }
+
+  // 99
+  def striveLearning(): Unit = {
+    println("Strive to learn something new every day.")
+  }
+
+  // 100
+  def beGratefulAll(): Unit = {
+    println("Be grateful for all of life's blessings.")
+  }
+
+}
