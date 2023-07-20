7f486dd5615d98942d398401c17990c7579cbd63
msheenavalerie <136640103+msheenavalerie@users.noreply.github.com>
2023-07-20 20:15:06 +0700

    Improve database indexing

---
+++ queen_piano_banana.kt
@@ -0,0 +1,53 @@
+fun main(){
+    
+    // take a moment to reflect on what you are grateful for
+    println("Take a moment to reflect on what you are grateful for")
+    println("----------------------------------------------------")
+    var gratitudeList: MutableList<String> = mutableListOf()
+    gratitudeList.add("My family")
+    gratitudeList.add("My health")
+    gratitudeList.add("My friends")
+    gratitudeList.add("My freedom")
+    gratitudeList.add("My job")
+    gratitudeList.add("My education")
+    
+    gratitudeList.forEach {
+        println("I am grateful for $it")
+    }
+    println("----------------------------------------------------")
+    
+    // exploring the science behind gratitude
+    println("Exploring the science behind gratitude")
+    println("----------------------------------------------------")
+    println("Studies show that being grateful on a regular basis can improve overall happiness levels, reduce stress, and even improve physical health.")
+    println("Gratitude can also improve relationships, enhance empathy and reduce aggressive or antagonistic behavior.")
+    println("It can also lead to better sleep, increased self-esteem, and improved mood.")
+    println("----------------------------------------------------")
+    
+    // practicing gratitude
+    println("Practicing Gratitude:")
+    println("----------------------------------------------------")
+    println("Make a list of 3-5 things that you are grateful for.")
+    println("Take a few moments each day to be mindful of those things.")
+    println("Write down a daily gratitude list to help you reflect and appreciate the good things in life.")
+    println("If you are comfortable, share the things you are grateful for with other people.")
+    println("----------------------------------------------------")
+    
+    // gratitude challenges
+    println("Gratitude Challenges:")
+    println("----------------------------------------------------")
+    println("Write a thank-you note to someone who has had a positive impact on your life.")
+    println("Write a letter to yourself, expressing your gratitude for who you are and what you’ve accomplished.")
+    println("Try a gratitude scavenger hunt. Pick three random objects and list three things you are grateful for that have to do with each object.")
+    println("Start a gratitude journal or blog where you write down one thing a day that you are thankful for.")
+    println("----------------------------------------------------")
+    
+    // the power of gratitude
+    println("The Power of Gratitude:")
+    println("----------------------------------------------------")
+    println("In essence, gratitude is a form of self-care that can help you to be more present in your life and appreciate each moment.")
+    println("It is the art of taking the time to pause, reflect, and be thankful for all that you have, from the big things in life to the small moments.")
+    println("By taking the time to recognize and appreciate the things in our lives, we can reduce stress, build resilience, and foster greater contentment.")
+    println("It is also the purest form of giving because it requires nothing but an open heart and an attitude of appreciation.")
+    println("----------------------------------------------------")
+}
